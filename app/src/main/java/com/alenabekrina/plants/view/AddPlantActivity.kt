142aa98afef5bd0039612560fc44e7e6b44b1887
Alena Bekrina <abekrina@gmail.com>
2018-11-20 17:02:33 +0100

    WIP add plant activity

--- app/src/main/java/com/alenabekrina/plants/view/AddPlantActivity.kt
+++ app/src/main/java/com/alenabekrina/plants/view/AddPlantActivity.kt
@@ -1,15 +1,21 @@
 package com.alenabekrina.plants.view
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.EditText
-import android.widget.TextView
 import com.alenabekrina.plants.R
+import kotlinx.android.synthetic.main.activity_add_plant.*
 
 class AddPlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_plant)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        numberpicker_watering_interval.minValue = 1
+        numberpicker_watering_interval.maxValue = 30
+
+        numberpicker_days_since_last_watering.minValue = 0
+        numberpicker_days_since_last_watering.maxValue = 30
     }
 }
