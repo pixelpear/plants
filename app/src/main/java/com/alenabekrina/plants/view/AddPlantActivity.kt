4e85fe773a68d0a6b5f3805bd5087fe53a07ceb1
Alena Bekrina <abekrina@gmail.com>
2018-11-21 17:14:42 +0100

    Better layout for AddPlantActivity

--- app/src/main/java/com/alenabekrina/plants/view/AddPlantActivity.kt
+++ app/src/main/java/com/alenabekrina/plants/view/AddPlantActivity.kt
@@ -3,7 +3,6 @@ package com.alenabekrina.plants.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.alenabekrina.plants.R
-import kotlinx.android.synthetic.main.activity_add_plant.*
 
 class AddPlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +10,5 @@ class AddPlantActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_plant)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        numberpicker_watering_interval.minValue = 1
-        numberpicker_watering_interval.maxValue = 30
-
-        numberpicker_days_since_last_watering.minValue = 0
-        numberpicker_days_since_last_watering.maxValue = 30
     }
 }
